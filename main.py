a0ab478a75a5eac05f01f3e6acda54f5348b5817
Shestakov Konstantin <shestakov.ks94@gmail.com>
2019-11-23 14:43:48 +0300

    refactoring and enchancements

--- main.py
+++ main.py
@@ -1,52 +1,62 @@
+from collections import defaultdict
 import datetime
 
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-def get_wines_from_file(filename):
-    products = dict()
-    product = dict()
+def get_product_file_lines(filename):
+    with open(filename) as f:
+        return f.read().strip().split('\n')
+
+
+def get_wines_from_file(product_file_lines):
     mapping = {
         'Название': 'name',
         'Сорт': 'grape',
         'Цена': 'price',
         'Картинка': 'image',
     }
-    with open(filename) as f:
-        products_file = f.read().strip().split('\n')
-    for line in products_file:
+    products = defaultdict(list)
+    product_subtitle = None
+    default_product_subtitle = 'Другое'
+    product = {}
+
+    for index, line in enumerate(product_file_lines):
         if line.startswith('#'):
-            product_line = line.split(' ', 1)[-1]
-            products[product_line] = products.setdefault(product_line, [])
-        elif line:
-            if line.startswith('Выгодное предложение'):
+            product_subtitle = line.split(' ', 1)[-1]
+        elif line.startswith('Выгодное предложение'):
                 product['profitable'] = True
-            else:
-                key, value = line.split(':')
-                product[mapping[key]] = value.strip()
-        elif not line and product:
-            products[product_line].append(product)
-            product = dict()
-    products[product_line].append(product)
+        elif line:
+            key, value = line.split(':')
+            product[mapping[key.strip()]] = value.strip()
+
+        if (not line and product) or (product and index == len(product_file_lines) - 1):
+            product_subtitle = product_subtitle or default_product_subtitle
+            products[product_subtitle].append(product)
+            product = {}
+
     return products
 
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+if __name__ == '__main__':
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-template = env.get_template('template.html')
+    template = env.get_template('template.html')
 
-products = get_wines_from_file('wine.txt')
-rendered_page = template.render(
-    years_since_founded=str(datetime.date.today().year - 1920),
-    products=products
-)
+    foundation_year = 1920
+    product_file_lines = get_product_file_lines('wine.txt')
+    products = get_wines_from_file(product_file_lines)
+    rendered_page = template.render(
+        years_since_founded=str(datetime.date.today().year - foundation_year),
+        products=products
+    )
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
